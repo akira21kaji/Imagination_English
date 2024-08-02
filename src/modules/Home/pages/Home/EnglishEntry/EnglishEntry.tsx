a3ee103604cc7ea42b993f0e02edb7d3090f97a0
akira21kaji <arkamiyama21@gmail.com[alias]>
2024-08-02 18:30:16 +0900

    画像生成したものをいきなり保存せずにモーダル表示→保存の流れを作りました

--- src/modules/Home/pages/Home/EnglishEntry/EnglishEntry.tsx
+++ src/modules/Home/pages/Home/EnglishEntry/EnglishEntry.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import { useWordsContext } from '@/src/lib/words/wordsContext';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import GenerateResultModal from '../../../components/GenerateResultModal';
 
 const EnglishEntry = () => {
   const {inputWord, setInputWord, generateExplanation, setGenerateExplanation} = useWordsContext() || {};
+  const [isModalOpen, setIsModalOpen] = useState(false);
   
   const handleGenerate = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -35,32 +37,10 @@ const EnglishEntry = () => {
       }
     }
   }
-  
+
   useEffect(() => {
-    const sendFirebase = async () => {
-
-      if(!generateExplanation) {
-        console.log('generateExplanation is not defined');
-        return;
-      }
-      
-      const firebaseResponse = await fetch('api/firebase',{
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          generateExplanation,
-        }),
-      });
-
-      if(!firebaseResponse.ok) { 
-        throw new Error('Network response was not ok');
-      }
-      
-    }
     if(generateExplanation) {
-      sendFirebase();
+      setIsModalOpen(true);
     }
   },[generateExplanation])
   
@@ -86,15 +66,13 @@ const EnglishEntry = () => {
             Generate
           </button>
       </form>
-      <div className='bg-neutral-600 p-4 rounded-lg m-4 w-3/5'>
-        <p className='text-white text-center'>
-          {generateExplanation 
-            ? typeof generateExplanation === 'string' 
-            ? generateExplanation 
-            : JSON.stringify(generateExplanation)
-            : '英単語を入力してください'}
-        </p>
+      {isModalOpen && 
+      <div 
+        className='flex justify-center items-center h-full w-full bg-black bg-opacity-50'
+        >
+        <GenerateResultModal setIsModalOpen={setIsModalOpen}/>
       </div>
+      }
     </>
 
 )
